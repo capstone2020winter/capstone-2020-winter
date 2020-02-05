f21d716ce276766d5a1a2774daf048e6602885ad
KyleZhaoningCai <zcai9@my.centennialcollege.ca>
2020-02-04 19:03:18 -0500

    Merged everything

--- src/app/budget-balance/budget-balance-routing.module.ts
+++ src/app/budget-balance/budget-balance-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
         children:[
           {
             path: '',
-            loadChildren: '../budget-balance/fixed-expense/fixed-expense.module#FixedExpensePageModule'
+            loadChildren: './fixed-expense/fixed-expense.module#FixedExpensePageModule'
           }
         ]
       },
@@ -23,7 +23,7 @@ const routes: Routes = [
         children:[
           {
             path: '',
-            loadChildren: '../budget-balance/variable-expense/variable-expense.module#VariableExpensePageModule'
+            loadChildren: './variable-expense/variable-expense.module#VariableExpensePageModule'
           }
         ]
       },
@@ -32,7 +32,7 @@ const routes: Routes = [
         children:[
           {
             path: '',
-            loadChildren: '../budget-balance/income/income.module#IncomePageModule'
+            loadChildren: './income/income.module#IncomePageModule'
           }
         ]
       },
@@ -41,7 +41,7 @@ const routes: Routes = [
         children:[
           {
             path: '',
-            loadChildren: '../budget-balance/summary/summary.module#SummaryPageModule'
+            loadChildren: './summary/summary.module#SummaryPageModule'
           }
         ]
       },
