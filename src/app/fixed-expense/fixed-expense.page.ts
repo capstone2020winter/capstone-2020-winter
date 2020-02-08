dea788aafc81e24f786cf588a6f72bbfdd0d7dfb
Veronika <shuichi4@gmail.com>
2020-02-08 11:50:23 -0500

    Body of the item is styled

--- src/app/fixed-expense/fixed-expense.page.ts
+++ src/app/fixed-expense/fixed-expense.page.ts
@@ -12,4 +12,30 @@ export class FixedExpensePage implements OnInit {
   ngOnInit() {
   }
 
+
+  formataDecimal(e: any, delimeter: string = '.', decimals: number = 2) {
+      console.log(e.target.value);
+      let a: any = e.target.value.split('');
+      let ns: string = '';
+      a.forEach((c: any) => {
+          if (!isNaN(c)) {
+              ns = ns + c;
+          }
+      });
+      ns = parseInt(ns).toString();
+      if (ns.length < (decimals + 1)) {
+          ns = ('0'.repeat(decimals + 1) + ns);
+          ns = ns.slice((decimals + 1) * -1);
+      }
+      let input = ns.split('');
+      let result = '';
+      for (let i = 0; i < input.length; i++) {
+          if (i == input.length - decimals) {
+              result = result + delimeter + input[i];
+          } else {
+              result = result + input[i];
+        }
+         }
+      e.value = result;
+  }
 }
